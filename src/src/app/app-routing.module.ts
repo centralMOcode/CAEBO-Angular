6c11e6327b31b062458538e512d88cdb136b089c
Isaac <iwp42910@ucmo.edu>
2020-12-22 15:58:34 -0600

    Mocked up an initial profile component and added it to the sidebar routing.

--- src/src/app/app-routing.module.ts
+++ src/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
 
   { path: '', component: LayoutComponent, children: [
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'profile', component: ProfileComponent }
   ]
   },
 
